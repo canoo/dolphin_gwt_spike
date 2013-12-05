197e733c57db504e9e29ecbeabd6d4c331ba8340
sanchling <steve.anchling>
2013-12-05 08:44:41 +0100

    Adapted to gradle directory convention

--- src/com/canoo/dolphingwtspike/mainApplication/client/MainApplicationServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package com.canoo.dolphingwtspike.mainApplication.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface MainApplicationServiceAsync {
-	void getMessage(String msg, AsyncCallback<String> async);
-}
