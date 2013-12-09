2a858ccd0077cc7bd8ff04950f979f006429aa38
Sven Ehrke <sven.ehrke@canoo.com>
2013-12-09 11:31:30 +0100

    first binding working

--- src/main/java/com/canoo/opendolphin/client/Bla.java
+++ src/main/java/com/canoo/opendolphin/client/Bla.java
@@ -38,6 +38,37 @@ public final class Bla extends JavaScriptObject {
 
 			// create named PM with attribute on the client side
 			var textAttribute  = new ClientAttribute("attrId");
+			var rangeAttribute = new ClientAttribute("range");
+			console.log("INIT PM");
+			dolphin.getClientDolphin().presentationModel(
+				"org.opendolphin.demo.Tutorial.modelId", undefined,
+				textAttribute, rangeAttribute
+			);
+
+			// send echo command on button click
+			var logActionButton = $doc.getElementById("logActionButton");
+			logActionButton.addEventListener("click", function () {
+				dolphin.getClientDolphin().send("org.opendolphin.demo.Tutorial.echo");
+			});
+
+			// bind text input field to pm textAttribute bidirectionally
+			var textInput = $doc.getElementById("textInput");
+			textInput.addEventListener("input", function () {
+				textAttribute.setValue(textInput.value);
+			});
+			textAttribute.on("valueChange", function (data) {
+				textInput.value = data.newValue;
+			});
+
+			// bind label to textAttribute
+			var label = $doc.getElementById("label");
+			textAttribute.on("valueChange", function (data) {
+				label.innerHTML = data.newValue;
+			});
+
+
+
+
 
 		});
 
