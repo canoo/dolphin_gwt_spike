fde4630731a48fe5273ec4ae6073bad449d776cc
Sven Ehrke <sven.ehrke@canoo.com>
2014-05-28 07:16:28 +0200

    attribute creation working

--- src/main/java/com/canoo/opendolphin/client/gwt/ClientDolphin.java
+++ src/main/java/com/canoo/opendolphin/client/gwt/ClientDolphin.java
@@ -1,6 +1,7 @@
 package com.canoo.opendolphin.client.gwt;
 
 import com.canoo.opendolphin.client.js.*;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
 
 import java.util.ArrayList;
@@ -9,11 +10,11 @@ import java.util.List;
 public class ClientDolphin {
 
     private final ClientDolphinJS clientDolphinJS;
-    private final ClientAttributeJS clientAttributeModule;
+    private final JavaScriptObject instance;
 
-    public ClientDolphin(ClientDolphinJS clientDolphinJS, final ClientAttributeJS clientAttributeModule) {
+    public ClientDolphin(ClientDolphinJS clientDolphinJS) {
         this.clientDolphinJS = clientDolphinJS;
-		this.clientAttributeModule = clientAttributeModule;
+		instance = clientDolphinJS.instance();
     }
 
     public void send(String commandName){
@@ -34,8 +35,20 @@ public class ClientDolphin {
 		});
     }
 
+    public ClientAttribute attribute(String propertyName, String qualifier, String value, String tag) {
+		ClientAttributeJS clientAttributeJS = clientDolphinJS.attribute(instance, propertyName, qualifier, value, tag);
+		return new ClientAttribute(clientAttributeJS);
+	}
+
     public PresentationModel presentationModel(String id, String type, String... clientAttributePropertyNames) {
 
+		for (String propertyName : clientAttributePropertyNames) {
+			JSLogger.log("ClientDolphin.presentationModel: creating attribute for: " + propertyName);
+			this.attribute(propertyName, null, null, null);
+			JSLogger.log("ClientDolphin.presentationModel: creating attribute done.");
+		}
+
+/*
         JsArray jsAttributes = ClientAttributeJS.createArray().cast();
         for (String propertyName : clientAttributePropertyNames) {
             jsAttributes.push(ClientAttributeJS.newClientAttributeJS(clientAttributeModule, propertyName));
@@ -43,6 +56,8 @@ public class ClientDolphin {
 
 		PresentationModelJS presentationModelJS = PresentationModelJS.newPresentationModelJS(clientDolphinJS, id, type, jsAttributes);
 		return new PresentationModel(presentationModelJS);
+*/
+		return null;
     }
 
     public ClientModelStore getClientModelStore() {
